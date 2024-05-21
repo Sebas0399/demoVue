f53b2b833ff48905ba8122ed59f6d82f56382e18
Sebas0399 <antony.aguay@gmail.com>
2024-05-20 22:27:53 -0500

    first commit

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,7 +1,7 @@
 //const API_URL = "https://daeapi.azurewebsites.net";
 //const API_URL="https://dae-6nst.onrender.com"
 //const API_URL = "https://apibitacora.onrender.com";
-const API_URL="http://localhost:3000"
+const API_URL=process.env.EXPRESS_APP_API_URL;
 export function getApiUrl(endpoint) {
     return `${API_URL}/${endpoint}`;
   }
